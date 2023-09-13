1d9f73aaad6243cce4bf50d2d022edf064abe61c
Vyacheslav Grevtsev <quemepasa@yandex.ru>
2023-09-13 09:32:19 +0300

    add tests for the Bun class

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,45 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class BunTest {
+    private Bun bun;
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: {0}, {1}")
+    public static Object[][] getSumData() {
+        return new Object[][] {
+                { "Флюоресцентная булка R2-D3", 988 },
+                { "Краторная булка N-200i", (float) 1250.75 },
+        };
+    }
+
+    @Before
+    public void setUp() {
+        bun = new Bun(name, price);
+    }
+
+    @Test
+    public void checkGetNameReturnName() {
+        String actualResult = bun.getName();
+        assertEquals("Неправильное имя булочки", name, actualResult);
+    }
+
+    @Test
+    public void checkGetPriceReturnPrice() {
+        float actualResult = bun.getPrice();
+        assertEquals("Неправильная цена булочки", price, actualResult, 0);
+    }
+}
